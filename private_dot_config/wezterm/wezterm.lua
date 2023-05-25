bc3e891bfe762a18c60a23ce0dad8af578d08f45
Dileep Kishore <k.dileep1994@gmail.com>
2023-05-25 17:22:14 -0400

    Add: Terminal configs: wezterm, alacritty

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require "wezterm"
+
+return {
+    force_reverse_video_cursor = true,
+    font = wezterm.font "Cascadia Code PL",
+    color_scheme = "Catppuccin Mocha",
+    enable_tab_bar = false,
+}
