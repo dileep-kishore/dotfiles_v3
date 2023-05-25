0a3858ba05e1b6ed1f48bb32c5b2d22a85570c77
Dileep Kishore <k.dileep1994@gmail.com>
2023-05-25 17:23:26 -0400

    Add: Zsh config (incomplete zinit/zplug not working)

---
+++ dot_zsh_aliases.zsh
@@ -0,0 +1,93 @@
+# Zsh aliases
+# To be sourced in .zshrc
+
+# nvim -> lvim
+alias nvim="lvim"
+
+# Use bat with dracule theme
+alias bat="bat --theme Dracula"
+
+# super user alias
+alias _="sudo "
+
+# clear aliases
+alias clear="clear -x"
+alias cls="clear"
+
+# rm alias
+alias rm="rm -i"
+
+# tmux aliases
+alias tnew="tmux new -s"
+alias ta="tmux attach -t"
+alias tdetach="tmux detach"
+alias tkill="tmux kill-session -t"
+alias tlist="tmux ls"
+
+# Launch ipython with vim keybindings
+alias vimipython="ipython --TerminalInteractiveShell.editing_mode=vi"
+
+# Git aliases
+alias gitplog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --graph --date=short"
+
+# lsd aliases
+alias l="lsd -lt --color always --icon always"
+alias ll="lsd -lhAt --color always --icon always"
+alias lg="exa -lag -s modified --git --color-scale"
+alias ls="lsd --color always --icon always"
+alias lt="lsd --tree --color always --icon always"
+alias la="lsd -la --color always --icon always"
+alias lsa="lsd -lah --color always --icon always"
+
+# List only folders
+alias folders="find . -maxdepth 1 -type d -print0 | xargs -0 du -sk | sort -rn"
+
+# disk aliases
+alias df="df -ahiT --total"
+alias du="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
+
+# mkdir alias
+alias mkdir="mkdir -pv"
+
+# users
+alias userlist="cut -d: -f1 /etc/passwd"
+
+# memory and process aliases
+alias free="free -mt"
+alias ps="ps auxf"
+alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+
+# wget and curl aliases
+alias wget="wget -c"
+alias myip="curl http://ipecho.net/plain; echo"
+
+# grep alias
+alias grep="grep --color=auto"
+
+# devour aliases
+alias vlc="devour vlc"
+alias mpv="devour mpv"
+alias zathura="devour zathura"
+
+# Gives version of programs installed
+alias has="curl -sL https://git.io/_has | bash -s"
+
+# Anaconda path export alias
+alias exportforge="export PATH=$HOME/mambaforge/bin:$PATH"
+
+# Chezmoi aliases
+alias cz="chezmoi"
+alias cza="chezmoi add"
+alias cze="chezmoi edit"
+alias czy="chezmoi apply"
+alias czd="chezmoi diff"
+
+# nnn aliases
+alias N="sudo -E nnn -dH"
+alias nnn="nnn -de"
+
+# todoist aliases
+alias todoist="todoist --color --namespace --indent --project-namespace"
+
+# vimclip alias
+alias vc="~/.dotfiles/bin/vimclip.sh"
---
+++ dot_zsh_functions.zsh
@@ -0,0 +1,94 @@
+# Zsh functions
+# To be sourced in .zshrc
+
+# Distrobox
+command_not_found_handle() {
+# don't run if not in a container
+  if [ ! -e /run/.containerenv ] && [ ! -e /.dockerenv ]; then
+    exit 127
+  fi
+  
+  distrobox-host-exec "${@}"
+}
+if [ -n "${ZSH_VERSION-}" ]; then
+  command_not_found_handler() {
+    command_not_found_handle "$@"
+ }
+fi
+
+# Display CSV file in a table in the terminal
+showcsv() { column -s, -t < "$1" | less -#2 -N -S; }
+
+# Convert softlink to hardlink
+tohardlink() { ln -f "$(readlink -m "$1")" "$1"; }
+
+# Recursively add files to git
+gitrecadd() { git ls-files "$1" | grep "$2" | xargs git add }
+
+# Shuffle order of input files and copy to destination
+shufflecopy() { shuf -zn10 -e "$1" | xargs -0 cp -vt "$2" }
+
+# Colored man pages
+man() {
+    env \
+      LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+      LESS_TERMCAP_md=$(printf "\e[1;31m") \
+      LESS_TERMCAP_me=$(printf "\e[0m") \
+      LESS_TERMCAP_se=$(printf "\e[0m") \
+      LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+      LESS_TERMCAP_ue=$(printf "\e[0m") \
+      LESS_TERMCAP_us=$(printf "\e[1;36m") \
+      man "$@"
+}
+export MANPAGER="sh -c 'col -bx | bat --theme Dracula -l man -p'"
+
+# Custom launch function
+launch() {
+    case $(file --mime-type "$@" -bL) in
+       # Check for the mimetype of your file (This is POSIX regex)
+       video/* | audio/* | image/gif)
+          # Launch using your favorite application
+          devour mpv "$@"
+          ;;
+       # So on and so forth...
+       application/pdf | application/postscript)
+          devour zathura "$@"
+          ;;
+       *) exit 1 ;;
+    esac
+}
+
+# Get current host related info.
+function sysinfo()
+{
+    echo -e "\n${BRed}System Informations:$NC " ; uname -a
+    echo -e "\n${BRed}Online User:$NC " ; w -hs |
+             cut -d " " -f1 | sort | uniq
+    echo -e "\n${BRed}Date :$NC " ; date
+    echo -e "\n${BRed}Server stats :$NC " ; uptime
+    echo -e "\n${BRed}Memory stats :$NC " ; free
+    echo -e "\n${BRed}Public IP Address :$NC " ; my_ip
+    echo -e "\n${BRed}Open connections :$NC "; netstat -pan --inet;
+    echo -e "\n${BRed}CPU info :$NC "; cat /proc/cpuinfo ;
+    echo -e "\n"
+}
+
+# Creates an archive (*.tar.gz) from given directory.
+function maketar() { tar cvzf "${1%%/}.tar.gz"  "${1%%/}/"; }
+
+# Create a ZIP archive of a file or folder.
+function makezip() { zip -r "${1%%/}.zip" "$1" ; }
+
+# Print current user's process list
+function my_ps() { ps $@ -u $USER -o pid,%cpu,%mem,bsdtime,command ; }
+
+# Scrot image to clipboard
+function scrot_clip() { scrot -s '/tmp/%F_%T.png' -e 'xclip -selection clip -t image/png -i "$f"' ; }
+
+# Function for easy access to wacom config
+function my_wacom() { /home/dileep/.dotfiles/bin/wacom_config.sh "$1" }
+
+# xrandr function to run when docking laptop
+function my_xrandr() {
+    xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-2-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2-2 --mode 1920x1080 --pos 3857x0 --rotate normal
+}
